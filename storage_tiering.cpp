31992e7d72c5107e4e278948918113a9601a3fd6
Kory Draughn <korydraughn@renci.org>
2019-12-04 17:23:25 -0500

    construct string

--- storage_tiering.cpp
+++ storage_tiering.cpp
@@ -846,7 +846,7 @@ namespace irods {
         auto& user = _comm.clientUser;
 
         if (user.authInfo.authFlag < LOCAL_PRIV_USER_AUTH) {
-            THROW(CAT_INSUFFICIENT_PRIVILEGE_LEVEL, "Cannot switch user [from=" + _user.userName +
+            THROW(CAT_INSUFFICIENT_PRIVILEGE_LEVEL, "Cannot switch user [from=" + std::string{_user.userName} +
                                                     ", to=" + _user_name + "]");
         }
 
